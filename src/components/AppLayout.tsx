cdcc9e6220dc92adadc2d4e3a8f585eac64ecf41
funtimes1 <65676039+funtimes1@users.noreply.github.com>
2021-11-05 20:44:11 +0100

    app layout complete (#6)
    Co-authored-by: funtimes1 <tamao.matsukawa@gmail.com>

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { x } from "@xstyled/styled-components";
+
+export const AppLayout: React.FunctionComponent = () => {
+  return (
+    <x.div
+      display="flex"
+      h="100vh"
+      flexDirection="column"
+      backgroundColor="green-200"
+    >
+      <x.div backgroundColor="red-200" h="20">
+        nav
+      </x.div>
+      <x.div display="flex" backgroundColor="blue-200" flex="1">
+        <x.div backgroundColor="yellow-200">
+          <x.div backgroundColor="blue-200" h="300" w="300">
+            calendar
+          </x.div>
+        </x.div>
+        <x.div
+          display="flex"
+          backgroundColor="green-200"
+          flex="1"
+          flexDirection="column"
+        >
+          <x.div backgroundColor="purple-200" h="100">
+            title
+          </x.div>
+          <x.div backgroundColor="red-200" h="300">
+            photo
+          </x.div>
+          <x.div backgroundColor="blue-200" flex="1">
+            events
+          </x.div>
+        </x.div>
+      </x.div>
+    </x.div>
+  );
+};
