56690f4f2fd8d5414e66e64f9a08a7a61e648a03
funtimes1 <tamao.matsukawa@gmail.com>
2022-04-03 19:57:19 +0200

    sorted out photo upload

--- src/components/journal-entry/AddJournalPhoto.tsx
+++ src/components/journal-entry/AddJournalPhoto.tsx
@@ -1,9 +1,11 @@
+import { arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 
 import { useForm } from "react-hook-form";
 import { uploadFile } from "../../databaseFirestore/uploadFile";
+import { db } from "../../firebase";
 import { Layout } from "../../theme/Layout.components";
-import { Photo } from "../../Types";
+import { JournalEntry, Photo } from "../../Types";
 import { formatDate } from "../../utils/formatDate";
 import { useCurrentUser } from "../hooks/UseCurrentUser";
 import { useDateStore } from "../useStore";
@@ -11,17 +13,17 @@ import { useDateStore } from "../useStore";
 //adds and displays photos
 export const AddJournalPhoto: React.FC = () => {
   const { selectedDate } = useDateStore();
-  const [error, setError] = useState(null);
   const user = useCurrentUser();
   const formattedDate = formatDate(selectedDate);
-  const journalEntriesPath = `users/${user?.uid}/journal-entries`;
-  // const addPhoto = uploadFile();
+  const journalEntryPath = `users/${user?.uid}/journal-entries`;
+  const journalRef = doc(db, journalEntryPath, formattedDate);
   const types = ["image/png", "image/jpeg"];
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm<Photo>({
     defaultValues: {
       url: "",
@@ -29,29 +31,50 @@ export const AddJournalPhoto: React.FC = () => {
     },
   });
 
-  const onSubmit = async (value: Photo, data) => {
+  const onSubmit = async (value: Photo) => {
     const { url, caption } = value;
-    const selectedFile = data.image[0];
     const photo = {
       url,
       caption,
     };
-    await uploadFile(selectedFile, photo);
+    // check to see if there is a journal and if not create a blank one
+    const journalSnap = await getDoc(journalRef);
+    if (!journalSnap.exists()) {
+      const newJournal: JournalEntry = {
+        date: formattedDate,
+        title: "",
+        notes: "",
+        photos: [],
+      };
+      await setDoc(journalRef, newJournal);
+    }
+    await updateDoc(journalRef, { photos: arrayUnion(photo) });
+
+    // after all that, update the journal to add the new photo using the arrayunion thing
   };
 
   return (
     <Layout.Column px py radius={10} bg="pink-200">
       Add photo
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("url")} type="file" name="image" />
+        {/* using onChange to set url photo before uploading it to firestore via onsubmit  */}
+        <input
+          onChange={async (event) => {
+            //using if here to ensure user is exists
+            if (user) {
+              console.log(event.target.files);
+              // takes the first element of the array
+              const [file] = event.target.files ?? [];
+              const url = await uploadFile(file, user, formattedDate);
+              setValue("url", url);
+            }
+          }}
+          type="file"
+          name="image"
+        />
         <input {...register("caption")} type="text" name="caption" />
         <button>Submit</button>
       </form>
     </Layout.Column>
   );
 };
-//click on upload file
-//choose photo in the correct format
-//save photo to the journal entry object in photos array (Save OR update photos array??)
-//update the photos array
-//display photos
--- src/databaseFirestore/uploadFile.tsx
+++ src/databaseFirestore/uploadFile.tsx
@@ -1,30 +1,27 @@
 import cuid from "cuid";
-import { doc, setDoc, getDoc, arrayUnion, updateDoc } from "firebase/firestore";
+import { User } from "firebase/auth";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { useCurrentUser } from "../components/hooks/UseCurrentUser";
-import { useDateStore } from "../components/useStore";
-import { db, storage } from "../firebase";
-import { Photo } from "../Types";
-import { formatDate } from "../utils/formatDate";
-export async function uploadFile(file: File, data: Photo) {
+import { storage } from "../firebase";
+
+//can't call hooks inside a function only inside a component, feed in the user and date as parameters(or arguments) rather than hooks
+export async function uploadFile(
+  file: File,
+  user: User,
+  formattedDate: string
+) {
   // creates reference to the file inside the default firebase storage when uploaded with the file name
 
-  const user = useCurrentUser();
-  const { selectedDate } = useDateStore();
-  const formattedDate = formatDate(selectedDate);
+  //changed from hook to a regular function by using .getState() because we cant call a hook in a reg func
+  // const { selectedDate } = useDateStore.getState();
+  // const formattedDate = formatDate(selectedDate);
   const id = cuid();
   const storageRef = ref(
     storage,
-    "the-images/" + id + formattedDate + file.name
+    `the-images/${user?.uid}/${formattedDate}/${id}`
   );
-
-  const journalEntryPath = `users/${user?.uid}/journal-entries`;
-  const journalRef = doc(db, journalEntryPath, formattedDate);
-
-  const result = await uploadBytes(storageRef, file).then((snapshot) => {
-    console.log("Uploaded a blob or file!", { result });
-  });
+  const result = await uploadBytes(storageRef, file);
+  console.log("result", result);
   const url = await getDownloadURL(storageRef);
-  await updateDoc(journalRef, { url: arrayUnion(url) });
+  console.log("url", url);
   return url;
 }
